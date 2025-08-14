c396fa55df2fb23ab50dc1b384d3187bd10742d6
kareemharvey <kareemjharvey@gmail.com>
2025-08-14 17:15:10 +0100

    fixed buttons and icons

--- app/src/main/java/com/example/mydialer/MainActivity.kt
+++ app/src/main/java/com/example/mydialer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mydialer
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,25 +18,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import com.example.mydialer.ui.theme.MyDIalerTheme
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Typography
-import androidx.compose.ui.Alignment
 import com.example.mydialer.ui.theme.Typography
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.painter.Painter
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,43 +63,87 @@ class MainActivity : ComponentActivity() {
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
                 val dial = Intent(Intent.ACTION_CALL, number.toUri())
-                startActivity(dial)
             }
 
         }.launch(Manifest.permission.CALL_PHONE)
-        /*setContent {
+        setContent {
+
             MyDIalerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    /*Greeting(
-                        name = "Android",
-                        modifier = Modifie.padding(innerPadding)
-                    )*/
-                }
+                Dialer()
+
             }
-        }*/
+        }
     }
 }
 
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyDIalerTheme {
-        Greeting("Android")
+fun DialButton(
+    main: String,
+    sub: String? = null,
+    icon: Painter? = null,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val dButtonModifier = Modifier
+        .fillMaxHeight()
+    Button(
+        onClick = onClick,
+        modifier = modifier.then(dButtonModifier)
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = main,
+                color = Color.White,
+                style = Typography.headlineLarge
+            )
+            if (icon != null) {
+                Spacer(Modifier.height(2.dp))
+                Icon(
+                    painter = icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            } else if (!sub.isNullOrBlank()) {
+                Spacer(Modifier.height(2.dp))
+                Text(text = sub)
+            }
+        }
     }
 }
+
+
+@Composable
+fun DialerRow(
+    left: @Composable () -> Unit,
+    middle: @Composable () -> Unit,
+    right: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .height(80.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        left()
+        Spacer(Modifier.width(8.dp))
+        middle()
+        Spacer(Modifier.width(8.dp))
+        right()
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun Dialer() {
+    var number by rememberSaveable { mutableStateOf("") }
+    val backIcon = painterResource(id = R.drawable.backspace_24dp_1f1f1f_fill0_wght400_grad0_opsz24)
+    val callIcon = painterResource(id = R.drawable.call_24dp_1f1f1f_fill0_wght400_grad0_opsz24)
+    val mailIcon = painterResource(id = R.drawable.voicemail_24dp_1f1f1f_fill0_wght400_grad0_opsz24)
 
     Box(
         modifier = Modifier
@@ -100,272 +161,138 @@ fun Dialer() {
                 Modifier.height(200.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    text = "test",
-                    modifier = Modifier,
-                    color = Color.Black,
-                    style = Typography.headlineLarge
+                Box(Modifier.fillMaxSize()) {
+                    Text(
+                        text = number.ifEmpty { " " },
+                        modifier = Modifier.align(Alignment.Center),
+                        color = Color.Black,
+                        fontSize = 58.sp,
+                        style = Typography.headlineLarge
 
-                )
+                    )
+                    Icon(
+                        painter = backIcon,
+                        contentDescription = "Delete",
+                        modifier = Modifier
+                            .size(48.dp)
+                            .align(Alignment.CenterEnd)
+                            .clickable{
+                                if (number.isNotEmpty()) number = number.dropLast(1)
+                            }
+                    )
+                }
             }
             HorizontalDivider()
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .height(80.dp)
-            ) {
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Text(
-                        text = "1",
-                        modifier = Modifier,
-                        color = Color.White,
-                        style = Typography.headlineLarge
+
+            DialerRow(
+                left = {
+                    DialButton(
+                        main = "1",
+                        icon = mailIcon,
+                        onClick = {number += "1"},
+                        modifier = Modifier.weight(1f)
                     )
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "2",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "ABC"
-                        )
-                    }
-
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "3",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "DEF"
-                        )
-                    }
-                }
-            }
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .height(80.dp)
-            ) {
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "4",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "GHI"
-                        )
-                    }
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "5",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "JKL"
-                        )
-                    }
-
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "6",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "MNO"
-                        )
-                    }
-                }
-            }
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .height(80.dp)
-            ) {
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "7",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "PQRS"
-                        )
-                    }
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "8",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "TUV"
-                        )
-                    }
-
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "9",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "WXYZ"
-                        )
-                    }
-                }
-            }
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .height(80.dp)
-            ) {
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Text(
-                        text = "*",
-                        modifier = Modifier,
-                        color = Color.White,
-                        style = Typography.headlineLarge
+                },
+                middle = {
+                    DialButton(
+                        main = "2",
+                        sub = "ABC",
+                        onClick = {number +="2"},
+                        modifier = Modifier.weight(1f)
                     )
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Column() {
-                        Text(
-                            text = "0",
-                            modifier = Modifier,
-                            color = Color.White,
-                            style = Typography.headlineLarge
-                        )
-                        Text(
-                            text = "+"
-                        )
-                    }
-
-                }
-                Spacer(Modifier.width(8.dp))
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                ) {
-                    Text(
-                        text = "#",
-                        modifier = Modifier,
-                        color = Color.White,
-                        style = Typography.headlineLarge
+                },
+                right = {
+                    DialButton(
+                        main = "3",
+                        sub = "DEF",
+                        onClick = {number += "3"},
+                        modifier = Modifier.weight(1f)
                     )
-                }
-            }
+                })
+            DialerRow(
+                left = {
+                    DialButton(
+                        main = "4",
+                        sub = "GHI",
+                        onClick = {number +="4"},
+                        modifier = Modifier.weight(1f)
+                    )
+                },
+                middle = {
+                    DialButton(
+                        main = "5",
+                        sub = "JKL",
+                        onClick = {number +="5"},
+                        modifier = Modifier.weight(1f)
+                    )
+                },
+                right = {
+                    DialButton(
+                        main = "6",
+                        sub = "MNO",
+                        onClick = {number +="6"},
+                        modifier = Modifier.weight(1f)
+                    )
+                })
+            DialerRow(
+                left = {
+                    DialButton(
+                        main = "7",
+                        sub = "PQRS",
+                        onClick = {number +="7"},
+                        modifier = Modifier.weight(1f)
+                    )
+                },
+                middle = {
+                    DialButton(
+                        main = "8",
+                        sub = "TUV",
+                        onClick = {number +="8"},
+                        modifier = Modifier.weight(1f)
+                    )
+                },
+                right = {
+                    DialButton(
+                        main = "9",
+                        sub = "WXYZ",
+                        onClick = {number +="9"},
+                        modifier = Modifier.weight(1f)
+                    )
+                })
+            DialerRow(
+                left = { DialButton(main = "*", onClick = {}, modifier = Modifier.weight(1f)) },
+                middle = {
+                    DialButton(
+                        main = "0",
+                        sub = "+",
+                        onClick = {number +="0"},
+                        modifier = Modifier.weight(1f)
+                    )
+                },
+                right = { DialButton(main = "#", onClick = {}, modifier = Modifier.weight(1f)) })
+
             Spacer(Modifier.height(16.dp))
-            Box (
-                modifier = Modifier.fillMaxWidth()
-                    .height(130.dp),
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp),
                 contentAlignment = Alignment.Center
-            ){
+            ) {
                 Button(
                     onClick = {},
-                    modifier = Modifier.width(200.dp).fillMaxHeight(),
-                   colors = ButtonDefaults.buttonColors(
-                       containerColor = Color(0xFF04650A),
-                       contentColor = Color.White
-                   )
-
-
-                ){
+                    modifier = Modifier
+                        .width(200.dp)
+                        .fillMaxHeight(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF04650A),
+                        contentColor = Color.White
+                    )
+                ) {
+                    Icon(
+                        painter = callIcon,
+                        contentDescription = "Call Icon",
+                        modifier = Modifier.size(24.dp)
+                    )
                     Text(
                         text = "CALL",
                         modifier = Modifier,
@@ -373,8 +300,6 @@ fun Dialer() {
                     )
                 }
             }
-
-
         }
     }
 }
